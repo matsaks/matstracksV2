b6618323f692f7e3701cea1017edf51b42ce6fd6
matsaks <matsen2000@gmail.com>
2024-04-29 09:10:40 +0200

    fixed locationbar

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,10 @@ export default function Home() {
   const [location, setLocation] = useState<LngLatLike>([6.327062, 62.463208]);
 
   const activities: ActivityType[] = data || [];
-  //const activities: ActivityType[] = [];
 
   if (isError) {
     toast.error("An error occurred while fetching activities.");
   }
-  //const isLoading = false;
 
   const updateLocation = (location: LngLatLike) => {
     setLocation(location);
@@ -32,16 +30,9 @@ export default function Home() {
         ) : (
           <ActivityMap activities={activities} center={location} />
         )}
-
         <Locationbar
-          loading={false}
-          className="sm:hidden mt-2 w-[100%]"
-          updateLocation={updateLocation}
-          activityType="heatmap"
-        />
-        <Locationbar
-          loading={false}
-          className="hidden sm:block mt-2 w-[50%]"
+          className="w-full mt-2"
+          loading={isLoading}
           updateLocation={updateLocation}
           activityType="heatmap"
         />
