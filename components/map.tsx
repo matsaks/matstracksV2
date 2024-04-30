0d39392fa6c014bf5a64915a4d85fcca89d0be37
matsaks <matsen2000@gmail.com>
2024-04-30 12:47:03 +0200

    feat: only renders the activities that are in the mapview
    now only the activities that are in the bounds of the map are rendered.
    This will improve the speed and loading time for the map

--- components/map.tsx
+++ components/map.tsx
@@ -3,7 +3,7 @@ import { decodePolyline } from "@/functions/decodePolyline";
 import { ActivityType } from "@/types/activity";
 import { Skeleton } from "./ui/skeleton";
 import { useEffect, useRef, useState } from "react";
-import mapboxgl, { LngLatLike, Map } from "mapbox-gl";
+import mapboxgl, { LngLatBounds, LngLatLike, Map } from "mapbox-gl";
 import { init } from "next/dist/compiled/webpack/webpack";
 
 enum Colorcodes {
@@ -27,6 +27,10 @@ interface IProps {
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
 export default function ActivityMap({ activities, zoom, center }: IProps) {
+  //const [bounds, setBounds] = useState<LngLatBounds>();
+  const [nw, setNw] = useState<number[]>();
+  const [se, setSe] = useState<number[]>();
+
   const ref = useRef<HTMLDivElement>(null);
 
   const [map, setMap] = useState<Map | null>(null);
@@ -43,50 +47,84 @@ export default function ActivityMap({ activities, zoom, center }: IProps) {
 
     _map.on("load", () => {
       setMap(_map);
+      const bounds = _map.getBounds();
+      setNw([bounds.getNorth(), bounds.getWest()]);
+      setSe([bounds.getSouth(), bounds.getEast()]);
     });
   }, []);
+
+  useEffect(() => {
+    if (map) {
+      map.on("moveend", () => {
+        const bounds = map.getBounds();
+        setNw([bounds.getNorth(), bounds.getWest()]);
+        setSe([bounds.getSouth(), bounds.getEast()]);
+      });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (map) {
+      activities.forEach((activity) => {
+        if (activity.sportType === "Workout") return;
+        if (
+          activity.startLatlng.length === 0 ||
+          activity.endLatlng.length === 0
+        )
+          return;
+
+        const startLatLng: LngLatLike = [
+          activity.startLatlng[1],
+          activity.startLatlng[0],
+        ];
+        const endLatLng: LngLatLike = [
+          activity.endLatlng[1],
+          activity.endLatlng[0],
+        ];
+        const isStartInside = map.getBounds().contains(startLatLng);
+        const isEndInside = map.getBounds().contains(endLatLng);
+
+        if (!isStartInside && !isEndInside) return;
+
+        const sourceID = activity.id.toString();
+
+        if (!map.getSource(sourceID)) {
+          map.addSource(activity.id.toString(), {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: decodePolyline(activity.summaryPolyline),
+              },
+            },
+          });
+          map.addLayer({
+            id: activity.id.toString(),
+            type: "line",
+            source: activity.id.toString(),
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
+            },
+            paint: {
+              "line-color":
+                Colorcodes[activity.sportType as keyof typeof Colorcodes],
+              "line-width": 5,
+              "line-opacity": 0.7,
+            },
+          });
+        }
+      });
+    }
+  }, [nw, se]);
 
   useEffect(() => {
     if (map) {
       map.flyTo({ center: center });
     }
   }, [center, map]);
-
-  useEffect(() => {
-    if (map) {
-      activities.forEach((activity) => {
-        if (activity.sportType === "Workout") return;
-        map.addSource(activity.id.toString(), {
-          type: "geojson",
-          data: {
-            type: "Feature",
-            properties: {},
-            geometry: {
-              type: "LineString",
-              coordinates: decodePolyline(activity.summaryPolyline),
-            },
-          },
-        });
-        map.addLayer({
-          id: activity.id.toString(),
-          type: "line",
-          source: activity.id.toString(),
-          layout: {
-            "line-join": "round",
-            "line-cap": "round",
-          },
-          paint: {
-            "line-color":
-              Colorcodes[activity.sportType as keyof typeof Colorcodes],
-            "line-width": 5,
-            "line-opacity": 0.7,
-          },
-        });
-      });
-    }
-  }, [activities, map]);
-
-  //if (loading) return <Skeleton className="w-[100%] h-[100%]" />;
 
   return <div id="map" ref={ref} className="w-[100%] h-[100%] rounded-xl" />;
 }
